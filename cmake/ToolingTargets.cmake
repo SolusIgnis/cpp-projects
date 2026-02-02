b637cbc06d077251c828f23cfd426cc16848ba93
SolusIgnis <gensolo@gmail.com>
2026-02-02 13:01:24 -0500

    parallelized targets format-check and tidy

--- cmake/ToolingTargets.cmake
+++ cmake/ToolingTargets.cmake
@@ -12,18 +12,32 @@ add_custom_target(format
 )
 
 # Target to run clang-format in checking mode.
+# Invokes per file for parallelism, incrementalism, and granularity.
 add_custom_target(format-check
-  COMMAND clang-format --dry-run --Werror --Wno-error=unknown
-          ${ALL_TOOLING_SOURCES}
-  COMMENT "Checking clang-format"
+  COMMENT "Checking clang-format on all sources"
 )
+foreach(src IN LISTS ALL_TOOLING_SOURCES)
+  add_custom_command(
+    TARGET format-check
+    COMMAND clang-format --dry-run --Werror --Wno-error=unknown ${src}
+    COMMENT "Running clang-tidy on ${src}"
+    VERBATIM
+  )
+endforeach()
 
 # Target to run clang-tidy if we're using clang as our compiler.
+# Invokes per file for parallelism, incrementalism, and granularity.
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   add_custom_target(tidy
-    COMMAND clang-tidy
-      --use-color -p ${CMAKE_BINARY_DIR}
-      ${ALL_TOOLING_SOURCES}
-    COMMENT "Running clang-tidy"
+    COMMENT "Running clang-tidy on all sources"
   )
+
+  foreach(src IN LISTS ALL_TOOLING_SOURCES)
+    add_custom_command(
+      TARGET tidy
+      COMMAND clang-tidy --use-color -p ${CMAKE_BINARY_DIR} ${src}
+      COMMENT "Running clang-tidy on ${src}"
+      VERBATIM
+    )
+  endforeach()
 endif()
