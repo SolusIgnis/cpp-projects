18dd84ec17610d01449490d5afd2e5181c863c0b
SolusIgnis <gensolo@gmail.com>
2026-02-02 18:08:51 -0500

    Updated ToolingTargets.cmake with a tidy-fast target to utilize the new scripts.

--- cmake/ToolingTargets.cmake
+++ cmake/ToolingTargets.cmake
@@ -6,38 +6,67 @@ get_property(ALL_TOOLING_SOURCES GLOBAL PROPERTY ALL_TOOLING_SOURCES)
 
 # Target to run clang-format (for real).
 add_custom_target(format
-  COMMAND clang-format -i
-          ${ALL_TOOLING_SOURCES}
+  COMMAND clang-format -i ${ALL_TOOLING_SOURCES}
   COMMENT "Running clang-format"
 )
 
 # Target to run clang-format in checking mode.
-# Invokes per file for parallelism, incrementalism, and granularity.
 add_custom_target(format-check
-  COMMENT "Checking clang-format on all sources"
+  TARGET format-check
+  COMMAND clang-format --dry-run --Werror --Wno-error=unknown ${ALL_TOOLING_SOURCES}
+  COMMENT "Checking clang-format"
+  VERBATIM
 )
-foreach(src IN LISTS ALL_TOOLING_SOURCES)
-  add_custom_command(
-    TARGET format-check
-    COMMAND clang-format --dry-run --Werror --Wno-error=unknown ${src}
-    COMMENT "Running clang-tidy on ${src}"
-    VERBATIM
-  )
-endforeach()
 
 # Target to run clang-tidy if we're using clang as our compiler.
 # Invokes per file for parallelism, incrementalism, and granularity.
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   add_custom_target(tidy
-    COMMENT "Running clang-tidy on all sources"
+    COMMAND clang-tidy
+      --use-color -p ${CMAKE_BINARY_DIR}
+      ${ALL_TOOLING_SOURCES}
+    COMMENT "Running clang-tidy"
   )
 
+  set(TIDY_STAMPS "")
+  set(TIDY_LOGS "")
+  set(TIDY_DIR "${CMAKE_BINARY_DIR}/tidy")
+  file(MAKE_DIRECTORY ${TIDY_DIR})
+  set(TIDY_LOG_DIR "${TIDY_DIR}/logs")
+  file(MAKE_DIRECTORY ${TIDY_LOG_DIR})
+  set(TIDY_STAMP_DIR "${TIDY_DIR}/stamps")
+  file(MAKE_DIRECTORY ${TIDY_STAMP_DIR})
+
   foreach(src IN LISTS ALL_TOOLING_SOURCES)
+    file(RELATIVE_PATH src_name ${CMAKE_SOURCE_DIR} ${src})
+    string(REPLACE "/" "_" src_name ${src_name})
+    string(REPLACE "\\" "_" src_name ${src_name})  # Windows path
+    string(SHA1 src_hash "${src}")
+    set(base_name "${src_name}-${src_hash}"
+    set(log_file "${TIDY_LOG_DIR}/${base_name}.log")
+    list(APPEND TIDY_LOGS ${log_file}) 
+    set(stamp_file "${TIDY_STAMP_DIR}/${base_name}.stamp")
+    list(APPEND TIDY_STAMPS ${stamp_file})
+
     add_custom_command(
-      TARGET tidy
-      COMMAND clang-tidy --use-color -p ${CMAKE_BINARY_DIR} ${src}
-      COMMENT "Running clang-tidy on ${src}"
-      VERBATIM
+      OUTPUT ${stamp_file}
+      BYPRODUCTS ${log_file}
+      COMMAND ${CMAKE_COMMAND}
+          -DBINARY_DIR=${CMAKE_BINARY_DIR}
+          -DSOURCE=${src}
+          -DLOG_FILE=${log_file}
+          -DSTAMP_FILE=${stamp_file}
+          -P ${CMAKE_SOURCE_DIR}/cmake/RunClangTidy.cmake
+      DEPENDS ${src}
+      COMMENT "clang-tidy ${src}"
     )
   endforeach()
+
+  add_custom_target(tidy-fast
+    DEPENDS ${TIDY_STAMPS}
+    COMMAND ${CMAKE_COMMAND}
+        -DSTAMPS=${TIDY_STAMPS}
+        -P ${CMAKE_SOURCE_DIR}/cmake/AggregateClangTidyResults.cmake
+    COMMENT "All clang-tidy checks completed"
+  )
 endif()
