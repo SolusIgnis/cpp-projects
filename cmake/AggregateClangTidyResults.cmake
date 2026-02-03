c42ad1ec541c43988d257093fb49027e738b3f16
SolusIgnis <gensolo@gmail.com>
2026-02-02 21:17:21 -0500

    Working on cmake scripts

--- cmake/AggregateClangTidyResults.cmake
+++ cmake/AggregateClangTidyResults.cmake
@@ -1,6 +1,25 @@
-execute_process(
-  COMMAND ${CMAKE_COMMAND} -E cat ${STAMPS}
-)
+if (NOT DEFINED STAMPS OR STAMPS STREQUAL "")
+  message(FATAL_ERROR "STAMPS variable MUST be set to a list of stamp files")
+endif()
+
+if (NOT DEFINED BUILD_DIR OR BUILD_DIR STREQUAL "")
+  message(FATAL_ERROR "BUILD_DIR variable MUST be set to the build directory")
+endif()
+
+if (DEFINED LOGS AND NOT LOGS STREQUAL "")
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E cat ${LOGS}
+    OUTPUT_VARIABLE all_logs
+  )
+  file(WRITE "${BUILD_DIR}/tidy/logs/tidy.log" "${all_logs}")
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E echo "${all_logs}"
+  )
+  foreach (log IN LISTS LOGS)
+    file(REMOVE "${log}")
+  endforeach()
+endif()
+
 foreach(stamp IN LISTS STAMPS)
   if (NOT EXISTS "${stamp}")
     message(FATAL_ERROR "Missing stamp file: ${stamp}")
