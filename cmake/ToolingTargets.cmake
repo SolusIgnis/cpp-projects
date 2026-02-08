f3fb516b9b59030efc1da57577bded3b7af6e385
Jeremy Murphy <gensolo@gmail.com>
2026-02-08 02:11:46 -0500

    Switched to run-clang-tidy script in CI (#7)
    * Added CMake targets `tidy-check-fast` and `tidy-fix-fast` to parallelize clang-tidy runs with the run-clang-tidy script. These targets can have the number of jobs configured by the TIDY_JOBS CMake variable, but it defaults to -j0.
    
    * Switched the CI configuration to use the `tidy-check-fast` target instead of the `tidy-check` target. This resulted in a >50% reduction in CI time for the tidy step.
    
    * Commented out `ExcludeHeaderFilterRegex` because CI script doesn't like it in `.clang-tidy`
    
    * Removed CMake scripts for per-file clang-tidy invocation and old `tidy-fast` target as it failed to provide acceptable performance.

--- cmake/ToolingTargets.cmake
+++ cmake/ToolingTargets.cmake
@@ -22,7 +22,6 @@ add_custom_target(format-check
 )
 
 # Target to run clang-tidy if we're using clang as our compiler.
-# tidy-fast Invokes per file for parallelism, incrementalism, and granularity.
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   add_custom_target(tidy-check
     COMMAND clang-tidy
@@ -33,64 +32,24 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   
   add_custom_target(tidy-fix
     COMMAND clang-tidy
-      -fix -p ${CMAKE_BINARY_DIR} ${ALL_TOOLING_SOURCES}
-    COMMENT "Checking code with clang-tidy in fix mode"
+      -fix --use-color -p ${CMAKE_BINARY_DIR} ${ALL_TOOLING_SOURCES}
+    COMMENT "Checking code with clang-tidy in \"fix\" mode"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   )
-
-  set(TIDY_STAMPS "")
-  set(TIDY_LOGS "")
-  set(COMPILE_COMMANDS "${CMAKE_BINARY_DIR}/compile_commands.json")
-  set(TIDY_CONFIG_FILE "${CMAKE_SOURCE_DIR}/.clang-tidy")
-  set(TIDY_DIR "${CMAKE_BINARY_DIR}/tidy")
-  file(MAKE_DIRECTORY ${TIDY_DIR})
-  set(TIDY_LOG_DIR "${TIDY_DIR}/logs")
-  file(MAKE_DIRECTORY ${TIDY_LOG_DIR})
-  set(TIDY_STAMP_DIR "${TIDY_DIR}/stamps")
-  file(MAKE_DIRECTORY ${TIDY_STAMP_DIR})
-
-  foreach(src IN LISTS ALL_TOOLING_SOURCES)
-#  foreach (i RANGE 0 3)
-#    list(GET ALL_TOOLING_SOURCES ${i} src)
-    file(RELATIVE_PATH src_name ${CMAKE_SOURCE_DIR} ${src})
-    string(REPLACE "/" "_" src_name ${src_name})
-    string(REPLACE "\\" "_" src_name ${src_name})  # Windows path
-    string(SHA1 src_hash "${src}")
-    set(base_name "${src_name}-${src_hash}")
-    set(log_file "${TIDY_LOG_DIR}/${base_name}.log")
-    list(APPEND TIDY_LOGS ${log_file}) 
-    set(stamp_file "${TIDY_STAMP_DIR}/${base_name}.stamp")
-    list(APPEND TIDY_STAMPS ${stamp_file})
-      
-    add_custom_command(
-      OUTPUT ${stamp_file}
-      BYPRODUCTS ${log_file}
-      COMMAND ${CMAKE_COMMAND}
-          -DBUILD_DIR=${CMAKE_BINARY_DIR}
-          -DSOURCE=${src}
-          -DLOG_FILE=${log_file}
-          -DSTAMP_FILE=${stamp_file}
-          -P ${CMAKE_SOURCE_DIR}/cmake/RunClangTidy.cmake
-      DEPENDS
-        ${src}
-        ${COMPILE_COMMANDS}
-        ${TIDY_CONFIG_FILE}
-      VERBATIM
-      COMMENT "clang-tidy ${src}"
-    )
-  endforeach()
-
-  message(STATUS "${TIDY_STAMPS}
   
-  ${TIDY_LOGS}")
-
-  add_custom_target(tidy-fast
-    DEPENDS ${TIDY_STAMPS}
-    COMMAND ${CMAKE_COMMAND}
-        -DBUILD_DIR=${CMAKE_BINARY_DIR}
-        -DSTAMPS=${TIDY_STAMPS}
-        -DLOGS=${TIDY_LOGS}
-        -P ${CMAKE_SOURCE_DIR}/cmake/AggregateClangTidyResults.cmake
-    COMMENT "All clang-tidy checks completed"
+  if(NOT DEFINED TIDY_JOBS)
+    set(TIDY_JOBS 0) # 0 means use all available cores for run-clang-tidy
+  endif()
+  
+  add_custom_target(tidy-check-fast
+    COMMAND run-clang-tidy -use-color -p ${CMAKE_BINARY_DIR} -j${TIDY_JOBS}
+    COMMENT "Checking code with clang-tidy (parallelized by run-clang-tidy script)"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  )
+  
+  add_custom_target(tidy-fix-fast
+    COMMAND run-clang-tidy -fix -use-color -p ${CMAKE_BINARY_DIR} -j${TIDY_JOBS}
+    COMMENT "Checking code with clang-tidy in \"fix\" mode (parallelized by run-clang-tidy script)"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   )
 endif()
