c98285af98bfe74fb09cb31cb34a584f84929393
SolusIgnis <gensolo@gmail.com>
2026-02-08 01:19:31 -0500

    Renamed tidy-py to tidy-check-fast, added corresponding tidy-fix-fast, and made both configurable via cmake variable.

--- cmake/ToolingTargets.cmake
+++ cmake/ToolingTargets.cmake
@@ -22,7 +22,6 @@ add_custom_target(format-check
 )
 
 # Target to run clang-tidy if we're using clang as our compiler.
-# tidy-fast Invokes per file for parallelism, incrementalism, and granularity.
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   add_custom_target(tidy-check
     COMMAND clang-tidy
@@ -33,14 +32,24 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   
   add_custom_target(tidy-fix
     COMMAND clang-tidy
-      -fix -p ${CMAKE_BINARY_DIR} ${ALL_TOOLING_SOURCES}
-    COMMENT "Checking code with clang-tidy in fix mode"
+      -fix --use-color -p ${CMAKE_BINARY_DIR} ${ALL_TOOLING_SOURCES}
+    COMMENT "Checking code with clang-tidy in \"fix\" mode"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   )
   
-  add_custom_target(tidy-py
-    COMMAND run-clang-tidy -p ${CMAKE_BINARY_DIR} -j0
-    COMMENT "Checking code with clang-tidy via script"
+  if(NOT DEFINED TIDY_JOBS)
+    set(TIDY_JOBS 0) # 0 means use all available cores for run-clang-tidy
+  endif()
+  
+  add_custom_target(tidy-check-fast
+    COMMAND run-clang-tidy --use-color -p ${CMAKE_BINARY_DIR} -j${TIDY_JOBS}
+    COMMENT "Checking code with clang-tidy (parallelized by run-clang-tidy script)"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+  )
+  
+  add_custom_target(tidy-fix-fast
+    COMMAND run-clang-tidy -fix --use-color -p ${CMAKE_BINARY_DIR} -j${TIDY_JOBS}
+    COMMENT "Checking code with clang-tidy in \"fix\" mode (parallelized by run-clang-tidy script)"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   )
 
