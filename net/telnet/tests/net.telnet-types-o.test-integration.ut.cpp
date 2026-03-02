c78ba83992285e397cddd68e0ac255c89fa6ea2c
SolusIgnis <gensolo@gmail.com>
2026-03-02 14:34:39 -0500

    mutable lambdas

--- net/telnet/tests/net.telnet-types-o.test-integration.ut.cpp
+++ net/telnet/tests/net.telnet-types-o.test-integration.ut.cpp
@@ -7,45 +7,45 @@ import std;
 
 using namespace ut;
 
-suite net_telnet_types_format_composition_tests = [] {
+suite net_telnet_types_format_composition_tests = [] mutable {
     using net::telnet::command;
     using net::telnet::negotiation_direction;
 
-    "command integrates with surrounding format text"_test = [] {
+    "command integrates with surrounding format text"_test = [] mutable {
         auto s = std::format("Received {}", command::nop);
         expect(eq(s, std::string{"Received NOP (0xf1)"}));
     };
 
-    "multiple commands format sequentially"_test = [] {
+    "multiple commands format sequentially"_test = [] mutable {
         auto s = std::format("{} {}", command::do_opt, command::dont_opt);
         expect(eq(s, std::string{"DO (0xfd) DONT (0xfe)"}));
     };
 
-    "command name-only format in composite string"_test = [] {
+    "command name-only format in composite string"_test = [] mutable {
         auto s = std::format("Cmd={:n}", command::brk);
         expect(eq(s, std::string{"Cmd=BRK"}));
     };
 
-    "negotiation_direction integrates in formatted output"_test = [] {
+    "negotiation_direction integrates in formatted output"_test = [] mutable {
         auto s = std::format("Direction: {}", negotiation_direction::remote);
         expect(eq(s, std::string{"Direction: remote"}));
     };
 };
 
-suite net_telnet_types_format_stability_tests = [] {
+suite net_telnet_types_format_stability_tests = [] mutable {
     using net::telnet::command;
 
-    "default format always contains 0x prefix"_test = [] {
+    "default format always contains 0x prefix"_test = [] mutable {
         auto s = std::format("{}", command::ga);
         expect(ne(s.find("0x"), std::string::npos));
     };
 
-    "hex-only format always starts with 0x"_test = [] {
+    "hex-only format always starts with 0x"_test = [] mutable {
         auto s = std::format("{:x}", command::ec);
         expect(eq(s.rfind("0x", 0), std::string::size_type{0}));
     };
 
-    "name-only format never contains 0x"_test = [] {
+    "name-only format never contains 0x"_test = [] mutable {
         auto s = std::format("{:n}", command::ayt);
         expect(eq(s.find("0x"), std::string::npos));
     };
--- net/telnet/tests/net.telnet-types-o.test-unit.ut.cpp
+++ net/telnet/tests/net.telnet-types-o.test-unit.ut.cpp
@@ -7,20 +7,20 @@ import std;
 
 using namespace ut;
 
-suite net_telnet_unit = [] {
-    "byte_t is exactly 1 byte"_test = [] {
+suite net_telnet_unit = [] mutable {
+    "byte_t is exactly 1 byte"_test = [] mutable {
         using net::telnet::byte_t;
         expect(eq(sizeof(byte_t), std::size_t{1}));
     };
 
-    "command formatting is stable"_test = [] {
+    "command formatting is stable"_test = [] mutable {
         using net::telnet::command;
 
         auto s = std::format("{}", command::will_opt);
         expect(eq(s, std::string{"WILL (0xfb)"}));
     };
 
-    "invalid format specifier throws format_error"_test = [] {
+    "invalid format specifier throws format_error"_test = [] mutable {
         using net::telnet::command;
 
         bool threw = false;
