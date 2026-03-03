d7b55372e3ed873fcba05642daeb3efc15f07bf5
SolusIgnis <gensolo@gmail.com>
2026-03-03 15:53:59 -0500

    tests for :options

---
+++ net/telnet/tests/net.telnet-options-o.test-unit.ut.cpp
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: Apache-2.0
+// Unit tests for net.telnet:options
+
+import net.telnet;
+import ut;
+import std;
+
+using namespace ut;
+
+suite net_telnet_option_unit_tests = [] {
+    using net::telnet::option;
+    using net::telnet::negotiation_direction;
+
+    "option stores id and name"_test = [] {
+        option opt{option::id_num::echo, "Echo"};
+
+        expect(eq(opt.get_id(), option::id_num::echo));
+        expect(eq(opt.get_name(), std::string{"Echo"}));
+    };
+
+    "default predicates reject"_test = [] {
+        option opt{option::id_num::echo};
+
+        expect(eq(opt.supports_local(), false));
+        expect(eq(opt.supports_remote(), false));
+    };
+
+    "always_accept predicate works"_test = [] {
+        option opt{
+            option::id_num::echo,
+            "Echo",
+            option::always_accept,
+            option::always_accept
+        };
+
+        expect(eq(opt.supports_local(), true));
+        expect(eq(opt.supports_remote(), true));
+    };
+
+    "supports(direction) dispatches correctly"_test = [] {
+        option opt{
+            option::id_num::echo,
+            "Echo",
+            option::always_accept,
+            option::always_reject
+        };
+
+        expect(eq(opt.supports(negotiation_direction::local), true));
+        expect(eq(opt.supports(negotiation_direction::remote), false));
+    };
+
+    "make_option sets predicates from booleans"_test = [] {
+        auto opt = option::make_option(
+            option::id_num::binary,
+            "Binary",
+            true,
+            false
+        );
+
+        expect(eq(opt.supports_local(), true));
+        expect(eq(opt.supports_remote(), false));
+    };
+
+    "subnegotiation defaults"_test = [] {
+        option opt{option::id_num::binary};
+
+        expect(eq(opt.supports_subnegotiation(), false));
+        expect(eq(opt.max_subnegotiation_size(),
+                  static_cast<std::size_t>(1024)));
+    };
+
+    "subnegotiation configuration honored"_test = [] {
+        option opt{
+            option::id_num::binary,
+            "Binary",
+            option::always_accept,
+            option::always_accept,
+            true,
+            static_cast<std::size_t>(4096)
+        };
+
+        expect(eq(opt.supports_subnegotiation(), true));
+        expect(eq(opt.max_subnegotiation_size(),
+                  static_cast<std::size_t>(4096)));
+    };
+
+    "three-way comparison based on id"_test = [] {
+        option a{option::id_num::binary};
+        option b{option::id_num::echo};
+
+        expect(eq((a < b), true));
+        expect(eq((b > a), true));
+        expect(eq((a == option::id_num::binary), true));
+    };
+
+    "implicit conversion to id_num works"_test = [] {
+        option opt{option::id_num::echo};
+        option::id_num id = opt;
+
+        expect(eq(id, option::id_num::echo));
+    };
+};
+
+int main() {}
