59238de57814bc6fce351ae6581ec8de7a0899af
SolusIgnis <gensolo@gmail.com>
2026-03-03 17:40:24 -0500

    Last Sync: 2026-03-03 17:40 (Mobile)

--- net/telnet/tests/net.telnet-options-o.test-unit.ut.cpp
+++ net/telnet/tests/net.telnet-options-o.test-unit.ut.cpp
@@ -10,6 +10,20 @@ using namespace ut;
 suite net_telnet_option_unit_tests = [] mutable {
     using net::telnet::option;
     using net::telnet::negotiation_direction;
+    using net::telnet::byte_t;
+
+    // ============================================================
+    // option::id_num enum structural properties
+    // ============================================================
+
+    "option::id_num enum structural guarantees"_test = [] mutable {
+        expect(eq(std::is_enum_v<option::id_num>, true));
+        expect(eq(std::is_same_v<std::underlying_type_t<option::id_num>, byte_t>, true));
+    };
+    
+    // ============================================================
+    // option tests
+    // ============================================================
 
     "option stores id and name"_test = [] mutable {
         option opt{option::id_num::echo, "Echo"};
