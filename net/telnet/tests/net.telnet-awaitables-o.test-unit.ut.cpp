554200c3e128e150ff4b19b133906199617af23d
SolusIgnis <gensolo@gmail.com>
2026-03-18 16:31:49 -0400

    Last Sync: 2026-03-18 16:31 (Mobile)

--- net/telnet/tests/net.telnet-awaitables-o.test-unit.ut.cpp
+++ net/telnet/tests/net.telnet-awaitables-o.test-unit.ut.cpp
@@ -23,10 +23,9 @@ suite net_telnet_awaitables_unit_tests = [] mutable {
 
     "tagged_awaitable constructs from awaitable"_test = [] mutable {
         coroutine_probe probe;
-        auto coro = []() -> test_task<void> { co_return; }();
-        coro.set_probe(&probe);
+        auto coro = []() -> test_task<void> { co_return; };
 
-        tagged_awaitable<tags::option_enablement_tag, void, test_task<void>> a{coro};
+        tagged_awaitable<tags::option_enablement_tag, void, test_task<void>> a{coro().set_probe(&probe)};
 
         expect(eq(probe.done, false));
         expect(eq(probe.destroyed, true));
