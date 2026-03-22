00e916e5191b128b2ea76a329671226e64af61f4
SolusIgnis <gensolo@gmail.com>
2026-03-22 01:06:26 -0400

    Last Sync: 2026-03-22 01:06 (Mobile)

--- net/telnet/tests/net.telnet-awaitables-o.test-unit.ut.cpp
+++ net/telnet/tests/net.telnet-awaitables-o.test-unit.ut.cpp
@@ -28,7 +28,7 @@ protected:
 
 public:
     constexpr trivial_awaiter_base() = default;
-    constexpr trivial_awaiter_base(T val) noexcept(std::is_nothrow_constructible_v<T>)
+    constexpr trivial_awaiter_base(T val) noexcept(std::is_nothrow_constructible_v<T>) requires (!is_void_v<T>)
         : storage_(val) {}
 
     constexpr auto await_resume() const noexcept {
