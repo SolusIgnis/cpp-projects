a5292659f146b0670ff490ffd8fc953d85a547df
SolusIgnis <gensolo@gmail.com>
2026-03-21 20:16:37 -0400

    Last Sync: 2026-03-21 20:16 (Mobile)

--- net/telnet/tests/net.telnet-awaitables-o.test-unit.ut.cpp
+++ net/telnet/tests/net.telnet-awaitables-o.test-unit.ut.cpp
@@ -156,7 +156,7 @@ suite net_telnet_awaitables_unit_tests = [] mutable {
         tester.operator()<asio::awaitable<int>>();
         tester.operator()<asio::awaitable<std::array<int, 4>>>();
     };
-#ifdef 0
+#if 0
 "check_alignment_glitch"_test = [] mutable {
     using task_t = test_task<void>;
     task_t tasks[2]; // Create an array
