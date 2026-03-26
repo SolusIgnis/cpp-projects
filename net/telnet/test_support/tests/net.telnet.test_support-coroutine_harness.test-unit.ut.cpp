f62aa9a4375de2266bb19b1725365533b5c0330c
SolusIgnis <gensolo@gmail.com>
2026-03-26 11:54:30 -0400

    Last Sync: 2026-03-26 11:54 (Mobile)

--- net/telnet/test_support/tests/net.telnet.test_support-coroutine_harness.test-unit.ut.cpp
+++ net/telnet/test_support/tests/net.telnet.test_support-coroutine_harness.test-unit.ut.cpp
@@ -478,8 +478,10 @@ suite as_task_adapter_tests = [] mutable {
         auto taskA = as_task<int>(echo_ready_awaiter{expected});
         taskA.set_probe(&probeA);
         
+        try {
         auto taskB = as_task<int>(as_task<int>(taskA));
         taskB.set_probe(&probeB);
+        } catch(...) { expect(eq(true, false)); }
 
         auto result = run(taskB);
         expect(eq(result, expected));
