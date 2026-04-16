c6076ba47305edcbca457d28ab53a7386168a946
SolusIgnis <gensolo@gmail.com>
2026-04-16 16:22:10 -0400

    Last Sync: 2026-04-16 16:22 (Mobile)

--- modules/base/functional/overload/tests/base.functional.overload.test-unit.ut.cpp
+++ modules/base/functional/overload/tests/base.functional.overload.test-unit.ut.cpp
@@ -224,7 +224,7 @@ suite overload_tests = [] mutable {
                 if (!ptr) throw std::logic_error("test tree node holds null pointer");
                 return std::visit(self, *ptr);
             }
-            [](this auto& self, std::tuple<node, node> children) {
+            [](this auto& self, std::tuple<auto, auto> children) {
                 auto [left, right] = children;
                 return std::visit(self, left.value) + std::visit(self, right.value);
             }
