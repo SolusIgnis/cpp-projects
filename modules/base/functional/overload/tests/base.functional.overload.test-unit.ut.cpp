098ccfdcb665b2a531c922a328fd12ef4b2f8b7a
SolusIgnis <gensolo@gmail.com>
2026-04-16 19:20:23 -0400

    Last Sync: 2026-04-16 19:20 (Mobile)

--- modules/base/functional/overload/tests/base.functional.overload.test-unit.ut.cpp
+++ modules/base/functional/overload/tests/base.functional.overload.test-unit.ut.cpp
@@ -231,19 +231,19 @@ suite overload_tests = [] mutable {
         };
         
         int i = 0;
-        auto tree = node{std::tuple{
-            std::make_unique<node>(std::tuple{
+        auto tree = node{std::move(std::tuple{
+            std::make_unique<node>(std::move(std::tuple{
                 std::make_unique<node>(++i),
-                std::make_unique<node>(std::tuple{
+                std::make_unique<node>(std::move(std::tuple{
                     std::make_unique<node>(++i),
                     std::make_unique<node>(++i)
-                })
-            }),
-            std::make_unique<node>(std::tuple{
+                }))
+            })),
+            std::make_unique<node>(std::move(std::tuple{
                 std::make_unique<node>(++i),
                 std::make_unique<node>(++i)
-            })
-        }};
+            }))
+        })};
         
         const int expected = (i * (i + 1)) / 2;
         expect(eq(std::visit(tree_sum, tree.value), expected));
