564a7573ba8296fdb1aa04c6fbaa47859d5a09a0
SolusIgnis <gensolo@gmail.com>
2026-07-05 22:58:26 -0400

    Last Sync: 2026-07-05 22:58 (Mobile)

--- modules/base/vocab/ptr/tests/base.vocab.ptr.test-integration.ut.cpp
+++ modules/base/vocab/ptr/tests/base.vocab.ptr.test-integration.ut.cpp
@@ -56,8 +56,8 @@ namespace {
             expect(eq(std::input_or_output_iterator<iterator_ptr<std::int32_t>>, true));
             expect(eq(std::input_iterator<iterator_ptr<std::int32_t>>, true));
             expect(eq(std::input_iterator<iterator_ptr<const std::int32_t>>, true));
-            expect(eq(std::output_iterator<iterator_ptr<std::int32_t>>, true));
-            expect(eq(std::output_iterator<iterator_ptr<const std::int32_t>>, false));
+            expect(eq(std::output_iterator<iterator_ptr<std::int32_t>, std::int32_t>, true));
+            expect(eq(std::output_iterator<iterator_ptr<const std::int32_t>, std::int32_t>, false));
             expect(eq(std::sized_sentinel_for<iterator_ptr<std::int32_t>, iterator_ptr<std::int32_t>>, true));
             expect(eq(std::forward_iterator<iterator_ptr<std::int32_t>>, true));
             expect(eq(std::bidirectional_iterator<iterator_ptr<std::int32_t>>, true));
