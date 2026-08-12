9d76d184680e01489ad9e4cc2117dde7380c0752
SolusIgnis <gensolo@gmail.com>
2026-08-11 23:57:51 -0400

    Last Sync: 2026-08-11 23:57 (Mobile)

--- modules/base/vocab/ptr/tests/base.vocab.ptr.test-unit.ut.cpp
+++ modules/base/vocab/ptr/tests/base.vocab.ptr.test-unit.ut.cpp
@@ -653,13 +653,13 @@ namespace {
 
         "dynamic_pointer_cast preserves null state"_test = [] mutable {
             test_each_pointer_type_with([]<template<typename> typename ConcretePtr>() {
-                if constexpr (pointer_test_traits<ConcretePtr>::is_nullable) {
+       /*         if constexpr (pointer_test_traits<ConcretePtr>::is_nullable) {
                     ConcretePtr<derived_type> source{nullptr};
 
                     auto result = dynamic_pointer_cast<base_type>(source);
 
                     expect(eq(result == nullptr, true));
-                }
+                }>/
             });
         };
 
@@ -719,7 +719,7 @@ namespace {
                 }
             });
         };
-        
+#if defined(__cpp_lib_start_lifetime_as)
         "start_lifetime_as alters pointee type"_test = [] mutable {
             test_each_pointer_type_with([]<template<typename> typename ConcretePtr>() {
                 struct origin_t {
@@ -768,7 +768,9 @@ namespace {
                 }
             });
         };
-
+#else
+#warning "std::start_lifetime_as not defined. Tests skipped."
+#endif
         //============================================================
         // Rebinding
         //============================================================
