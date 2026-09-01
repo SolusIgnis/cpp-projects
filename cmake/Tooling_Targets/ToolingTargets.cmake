3cea911fd94f7790f533c0f27eb69ff0c10e5456
SolusIgnis <gensolo@gmail.com>
2026-09-01 13:22:55 -0400

    Last Sync: 2026-09-01 13:22 (Mobile)

--- cmake/Tooling_Targets/ToolingTargets.cmake
+++ cmake/Tooling_Targets/ToolingTargets.cmake
@@ -47,13 +47,13 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
   endif()  
 
   add_custom_target(tidy-check-fast
-    COMMAND run-clang-tidy -use-color -p ${CMAKE_BINARY_DIR} -j${TIDY_JOBS} -source-filter "${TIDY_SOURCE_FILTER_REGEX}" -allow-no-checks
+    COMMAND run-clang-tidy -use-color -p ${CMAKE_BINARY_DIR} -j${TIDY_JOBS} -source-filter ${TIDY_SOURCE_FILTER_REGEX} -allow-no-checks
     COMMENT "Checking code with clang-tidy (parallelized by run-clang-tidy script)"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   )
   
   add_custom_target(tidy-fix-fast
-    COMMAND run-clang-tidy -fix -use-color -p ${CMAKE_BINARY_DIR} -j${TIDY_JOBS} -source-filter "${TIDY_SOURCE_FILTER_REGEX}" -allow-no-checks
+    COMMAND run-clang-tidy -fix -use-color -p ${CMAKE_BINARY_DIR} -j${TIDY_JOBS} -source-filter ${TIDY_SOURCE_FILTER_REGEX} -allow-no-checks
     COMMENT "Checking code with clang-tidy in \"fix\" mode (parallelized by run-clang-tidy script)"
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   )
