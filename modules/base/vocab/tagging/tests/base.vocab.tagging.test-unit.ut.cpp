584ab7d8936316ceebf2eab842cdebb1d76cefea
SolusIgnis <gensolo@gmail.com>
2026-09-08 12:32:01 -0400

    Last Sync: 2026-09-08 12:32 (Mobile)

--- modules/base/vocab/tagging/tests/base.vocab.tagging.test-unit.ut.cpp
+++ modules/base/vocab/tagging/tests/base.vocab.tagging.test-unit.ut.cpp
@@ -252,7 +252,7 @@ namespace {
 
         "moving and forwarding"_test = [] mutable {
             using tagged_t = base::vocab::tagged_boundary<test_tag, std::int32_t>;
-            constexpr auto consumer  = [](tagged_t arg) { std::int32_t val = std::move(arg); return val * val; }
+            constexpr auto consumer  = [](tagged_t arg) { std::int32_t val = std::move(arg); return val * val; };
             constexpr auto mover     = []<typename T>(std::add_rvalue_reference_t<T> arg) { return consumer(std::move(arg)); };
             constexpr auto forwarder = []<typename Arg>(Arg&& arg) { return mover(std::forward<Arg>(arg)); };
 
