b3b1237c8bf9ac75f71ff0114a9eaa4cd5924384
SolusIgnis <gensolo@gmail.com>
2026-09-12 20:47:24 -0400

    Fetching CPM.cmake

---
+++ cmake/FetchCPM.cmake
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2026 Jeremy Murphy and any Contributors
+#
+# FetchCPM.cmake
+#
+# Fetches CPM and includes it. 
+
+include_guard(GLOBAL)
+
+set(CPM_VERSION "0.43.1")
+set(CPM_HASH "1c40fc102ce9625d7de7eb14f541cab30cc3138dca627f0b0ec40293ce6c2934")
+
+set(CPM_URL "https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_VERSION}/CPM.cmake")
+set(CPM_PATH "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_VERSION}.cmake")
+
+if(NOT EXISTS "${cpm_file}")
+  file(DOWNLOAD
+       "${CPM_URL}"
+       "${CPM_PATH}"
+       EXPECTED_HASH "SHA256=${CPM_HASH}"
+       TLS_VERIFY ON
+  )
+endif()
+
+include("${CPM_PATH}")
