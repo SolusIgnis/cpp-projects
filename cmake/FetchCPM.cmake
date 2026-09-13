152e170724d5d9076bce47f8fda8f005d4ddca0f
SolusIgnis <gensolo@gmail.com>
2026-09-12 21:01:34 -0400

    Comments

--- cmake/FetchCPM.cmake
+++ cmake/FetchCPM.cmake
@@ -7,13 +7,15 @@
 
 include_guard(GLOBAL)
 
+# Current as of 2026-09-12
 set(CPM_VERSION "0.43.1")
+# SHA-256 Hash per GitHub Asset Metadata
 set(CPM_HASH "1c40fc102ce9625d7de7eb14f541cab30cc3138dca627f0b0ec40293ce6c2934")
 
 set(CPM_URL "https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_VERSION}/CPM.cmake")
 set(CPM_PATH "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_VERSION}.cmake")
 
-if(NOT EXISTS "${cpm_file}")
+if(NOT EXISTS "${CPM_PATH}")
   file(DOWNLOAD
        "${CPM_URL}"
        "${CPM_PATH}"
