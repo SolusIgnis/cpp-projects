17da55495a0e4af846292948c0f2a4283812a96d
SolusIgnis <gensolo@gmail.com>
2026-09-24 15:31:11 -0400

    Last Sync: 2026-09-24 15:31 (Mobile)

--- modules/base/vocab/ptr/tests/base.vocab.ptr-const_pointer_cast.test-unit.qlibs-ut.cpp
+++ modules/base/vocab/ptr/tests/base.vocab.ptr-const_pointer_cast.test-unit.qlibs-ut.cpp
@@ -19,48 +19,47 @@ int main()
     //============================================================
 
     //NOLINTBEGIN(misc-const-correctness): Readability suffers with const correctness in these tests.
-        "const_pointer_cast alters pointee cv-qualifications"_test = [] mutable {
-            test_each_pointer_type_with([]<template<typename> typename ConcretePtr> {
-                const auto test_cast = []<typename Source, typename Destination> {
-                    Source value{};
+    "const_pointer_cast alters pointee cv-qualifications"_test = [] mutable {
+        test_each_pointer_type_with([]<template<typename> typename ConcretePtr> {
+            const auto test_cast = []<typename Source, typename Destination> {
+                Source value{};
 
-                    auto source = base::vocab::pointer_to<ConcretePtr>(value);
-                    auto result = const_pointer_cast<Destination>(source);
+                auto source = base::vocab::pointer_to<ConcretePtr>(value);
+                auto result = const_pointer_cast<Destination>(source);
 
-                    expect(eq(std::same_as<decltype(result), ConcretePtr<Destination>>, true));
-                    expect(eq(result.get() == std::addressof(value), true));
-                };
+                expect(eq(std::same_as<decltype(result), ConcretePtr<Destination>>, true));
+                expect(eq(result.get() == std::addressof(value), true));
+            };
 
-                test_cast.template operator()<std::int32_t, std::int32_t>();
-                test_cast.template operator()<std::int32_t, const std::int32_t>();
-                test_cast.template operator()<std::int32_t, volatile std::int32_t>();
-                test_cast.template operator()<std::int32_t, const volatile std::int32_t>();
-                test_cast.template operator()<const std::int32_t, std::int32_t>();
-                test_cast.template operator()<const std::int32_t, const std::int32_t>();
-                test_cast.template operator()<const std::int32_t, volatile std::int32_t>();
-                test_cast.template operator()<const std::int32_t, const volatile std::int32_t>();
-                test_cast.template operator()<volatile std::int32_t, std::int32_t>();
-                test_cast.template operator()<volatile std::int32_t, const std::int32_t>();
-                test_cast.template operator()<volatile std::int32_t, volatile std::int32_t>();
-                test_cast.template operator()<volatile std::int32_t, const volatile std::int32_t>();
-                test_cast.template operator()<const volatile std::int32_t, std::int32_t>();
-                test_cast.template operator()<const volatile std::int32_t, const std::int32_t>();
-                test_cast.template operator()<const volatile std::int32_t, volatile std::int32_t>();
-                test_cast.template operator()<const volatile std::int32_t, const volatile std::int32_t>();
-            });
-        };
+            test_cast.template operator()<std::int32_t, std::int32_t>();
+            test_cast.template operator()<std::int32_t, const std::int32_t>();
+            test_cast.template operator()<std::int32_t, volatile std::int32_t>();
+            test_cast.template operator()<std::int32_t, const volatile std::int32_t>();
+            test_cast.template operator()<const std::int32_t, std::int32_t>();
+            test_cast.template operator()<const std::int32_t, const std::int32_t>();
+            test_cast.template operator()<const std::int32_t, volatile std::int32_t>();
+            test_cast.template operator()<const std::int32_t, const volatile std::int32_t>();
+            test_cast.template operator()<volatile std::int32_t, std::int32_t>();
+            test_cast.template operator()<volatile std::int32_t, const std::int32_t>();
+            test_cast.template operator()<volatile std::int32_t, volatile std::int32_t>();
+            test_cast.template operator()<volatile std::int32_t, const volatile std::int32_t>();
+            test_cast.template operator()<const volatile std::int32_t, std::int32_t>();
+            test_cast.template operator()<const volatile std::int32_t, const std::int32_t>();
+            test_cast.template operator()<const volatile std::int32_t, volatile std::int32_t>();
+            test_cast.template operator()<const volatile std::int32_t, const volatile std::int32_t>();
+        });
+    };
 
-        "const_pointer_cast preserves null state"_test = [] mutable {
-            test_each_pointer_type_with([]<template<typename> typename ConcretePtr> {
-                if constexpr (pointer_test_traits<ConcretePtr>::is_nullable) {
-                    ConcretePtr<std::int32_t> source{nullptr};
+    "const_pointer_cast preserves null state"_test = [] mutable {
+        test_each_pointer_type_with([]<template<typename> typename ConcretePtr> {
+            if constexpr (pointer_test_traits<ConcretePtr>::is_nullable) {
+                ConcretePtr<std::int32_t> source{nullptr};
 
-                    const auto result = const_pointer_cast<const std::int32_t>(source);
+                const auto result = const_pointer_cast<const std::int32_t>(source);
 
-                    expect(eq(result == nullptr, true));
-                }
-            });
-        };
+                expect(eq(result == nullptr, true));
+            }
+        });
     };
     //NOLINTEND(misc-const-correctness)
 }
